c555e6a5fb37ed8901f6bdb6cb9e802c1ab8dc42
juanaugusto96 <juanaugustoroldan7@gmail.com>
2023-12-13 19:02:52 -0300

    desafio-node

---
+++ src/app.js
@@ -0,0 +1,48 @@
+import express from "express";
+import { ProductManager } from "./ProductManager.js";
+
+const app = express();
+const PORT = 8080;
+const productManager = new ProductManager("productos.json");
+
+let productos = [];
+
+app.get("/", (_req, res) => {
+  res.send("hola mundo");
+});
+
+app.get("/productos", async (req, res) => {
+  const { limit } = req.query;
+  try {
+    let response = await productManager.getProducts();
+    if (limit) {
+      let tempArray = response.filter((dat, index) => index < limit);
+      /* let tempArray = response.map((dat, index) => {
+        return index < limit && dat;
+      });
+      */
+      res.json({ data: tempArray, limit: limit, quantity: tempArray.length });
+    } else {
+      res.json({ data: response, limit: false, quantity: response.length });
+    }
+  } catch (err) {
+    console.log(err);
+  }
+});
+app.get("/productos/:pid", async (req, res) => {
+  const { pid } = req.params;
+
+  let product = await productManager.getProductById(parseInt(pid));
+
+  if (product) {
+    res.json({ message: "success", data: product });
+  } else {
+    res.json({
+      message: "el producto solicitado no existe",
+    });
+  }
+});
+
+app.listen(PORT, () => {
+    console.log("Servidor está corriendo en el puerto " + PORT);
+  });
