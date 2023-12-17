b6617c0ec000d55b9768697f366220eb5fe5d808
juanaugusto96 <juanaugustoroldan7@gmail.com>
2023-12-17 16:51:57 -0300

    carrito

--- src/app.js
+++ src/app.js
@@ -1,48 +1,14 @@
+
 import express from "express";
-import { ProductManager } from "./ProductManager.js";
+import productsRouter from "./routes/products.js";
+import cartsRouter from "./routes/carts.js";
 
 const app = express();
 const PORT = 8080;
-const productManager = new ProductManager("productos.json");
 
-let productos = [];
-
-app.get("/", (_req, res) => {
-  res.send("hola mundo");
-});
-
-app.get("/productos", async (req, res) => {
-  const { limit } = req.query;
-  try {
-    let response = await productManager.getProducts();
-    if (limit) {
-      let tempArray = response.filter((dat, index) => index < limit);
-      /* let tempArray = response.map((dat, index) => {
-        return index < limit && dat;
-      });
-      */
-      res.json({ data: tempArray, limit: limit, quantity: tempArray.length });
-    } else {
-      res.json({ data: response, limit: false, quantity: response.length });
-    }
-  } catch (err) {
-    console.log(err);
-  }
-});
-app.get("/productos/:pid", async (req, res) => {
-  const { pid } = req.params;
-
-  let product = await productManager.getProductById(parseInt(pid));
-
-  if (product) {
-    res.json({ message: "success", data: product });
-  } else {
-    res.json({
-      message: "el producto solicitado no existe",
-    });
-  }
-});
+app.use("/products", productsRouter);
+app.use("/carts", cartsRouter);
 
 app.listen(PORT, () => {
-    console.log("Servidor está corriendo en el puerto " + PORT);
-  });
+  console.log("Servidor está corriendo en el puerto " + PORT);
+});
